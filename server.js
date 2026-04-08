334d799b85e7ce791e25ae672755954096bc4d7b
magit notif <231088448+godsownf@users.noreply.github.com>
2026-04-08 19:40:40 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,40 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 5000;
+const HOST = '0.0.0.0';
+
+const mimeTypes = {
+  '.html': 'text/html',
+  '.js': 'application/javascript',
+  '.css': 'text/css',
+  '.png': 'image/png',
+  '.ico': 'image/x-icon',
+  '.json': 'application/json',
+};
+
+const server = http.createServer((req, res) => {
+  let filePath = req.url === '/' ? '/popup.html' : req.url;
+  filePath = path.join(__dirname, filePath);
+
+  const ext = path.extname(filePath);
+  const contentType = mimeTypes[ext] || 'text/plain';
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('Not found');
+      return;
+    }
+    res.writeHead(200, {
+      'Content-Type': contentType,
+      'Cache-Control': 'no-cache',
+    });
+    res.end(data);
+  });
+});
+
+server.listen(PORT, HOST, () => {
+  console.log(`Fingerprint Spoofer extension preview running at http://${HOST}:${PORT}`);
+});
